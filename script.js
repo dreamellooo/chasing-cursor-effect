6a5c69cf504711f2a6fb1a050274321cb37880b1
chnrlotte <charlotte.jdh@gmail.com>
2024-04-26 22:51:48 +0800

    removed second section

--- script.js
+++ script.js
@@ -1,44 +1,3 @@
-function locoScroll() {
-  gsap.registerPlugin(ScrollTrigger);
-
-  // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
-
-  const locoScroll = new LocomotiveScroll({
-    el: document.querySelector("#main"),
-    smooth: true,
-  });
-  // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
-  locoScroll.on("scroll", ScrollTrigger.update);
-
-  // tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
-  ScrollTrigger.scrollerProxy("#main", {
-    scrollTop(value) {
-      return arguments.length
-        ? locoScroll.scrollTo(value, 0, 0)
-        : locoScroll.scroll.instance.scroll.y;
-    }, // we don't have to define a scrollLeft because we're only scrolling vertically.
-    getBoundingClientRect() {
-      return {
-        top: 0,
-        left: 0,
-        width: window.innerWidth,
-        height: window.innerHeight,
-      };
-    },
-    // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
-    pinType: document.querySelector("#main").style.transform
-      ? "transform"
-      : "fixed",
-  });
-
-  // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll.
-  ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
-
-  // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
-  ScrollTrigger.refresh();
-}
-locoScroll();
-
 function cursorEffect() {
   var page1Content = document.querySelector("#page1-content");
   var cursor = document.querySelector("#cursor");
@@ -65,34 +24,3 @@ function cursorEffect() {
   });
 }
 cursorEffect();
-
-function page2Animation() {
-    let startValue = "top 47%";
-    let endValue = "top 45%";
-  
-    // Check if the device width is less than or equal to 767px
-    if (window.innerWidth <= 767) {
-      startValue = "top 100%"; // New start value for mobile devices
-      endValue = "top 100%"; // New end value for mobile devices
-    }
-  
-    gsap.from(".elem h2", {
-      y: 100,
-      stagger: 0.2,
-      duration: 1,
-      scrollTrigger: {
-        trigger: "#page2",
-        scroller: "#main",
-        start: startValue,
-        end: endValue,
-        // markers: true,
-        scrub: 2,
-      },
-    });
-  }
-  
-  // Call the function when the page loads
-  page2Animation();
-  
-  // Recalculate start and end values when the window is resized
-  window.addEventListener("resize", page2Animation);
